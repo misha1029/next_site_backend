1f64559b09d3d70a90d586cc07646fcf0bf12a76
Misha <sherstoun001@gmail.com>
2022-04-18 17:55:35 +0300

    create CRUD users, comments, posts

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user/entities/user.entity';
+import { PostModule } from './post/post.module';
+import { PostEntity } from './post/entities/post.entity';
+import { CommentModule } from './comment/comment.module';
+import { CommentEntity } from './comment/entities/comment.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'news_site',
+      entities: [UserEntity, PostEntity, CommentEntity],
+      synchronize: true,
+    }),
+    UserModule,
+    PostModule,
+    CommentModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,10 @@
+import { IsEmail, Length } from 'class-validator';
+
+export class CreateUserDto {
+  @Length(3)
+  fullName: string;
+  @IsEmail(undefined, { message: 'Неверная почта' })
+  email: string;
+  @Length(6, 32, { message: 'Пароль должен состоять минимум из 6 символов' })
+  password?: string;
+}
